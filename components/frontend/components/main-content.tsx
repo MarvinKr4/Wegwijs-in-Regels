271bc2e72c31a97f33484020c1867632c4adeb5e
BijlsmaJL <114646214+BijlsmaJL@users.noreply.github.com>
2025-10-28 13:53:13 +0100

    adding component folder

---
+++ components/frontend/components/main-content.tsx
@@ -0,0 +1,30 @@
+import { ChatView } from "./chat-view";
+import { InitialHomepageView } from "./initial-homepage-view";
+import { Message } from "@/lib/types";
+
+interface MainContentProps {
+  messages: Message[];
+  handleNewChat: () => void;
+  chatStarted: boolean;
+}
+
+// Main content component that changes based on chat state
+export const MainContent = ({
+  messages,
+  handleNewChat,
+  chatStarted,
+}: MainContentProps) => {
+  return (
+    <div
+      className={`flex flex-col overflow-hidden transition-all duration-500 ${
+        chatStarted ? "max-h-full h-full" : "max-h-[800px] h-fit"
+      }`}
+    >
+      {chatStarted ? (
+        <ChatView messages={messages} handleNewChat={handleNewChat} />
+      ) : (
+        <InitialHomepageView />
+      )}
+    </div>
+  );
+};
---
+++ components/frontend/components/source-tag.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SourceTag = () => {
+  return (
+        <div>SourceTag</div>
+  )
+}
